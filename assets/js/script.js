fd75e9d1fa3500dae297e30f34552aa1b6c7cd81
aryadilas <44058486+aryadilas@users.noreply.github.com>
2021-09-15 09:31:58 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,24 @@ const btnpendidikan = document.querySelector('#pendidikanbtn');
 const btnkontak = document.querySelector('#kontakbtn');
 const btnkeahlian = document.querySelector('#keahlianbtn');
 
+
 btnhome.addEventListener('click', function(){
 	btnhome.classList.add('onpage');
 	btnprestasi.classList.remove('onpage');
 	btnpendidikan.classList.remove('onpage');
 	btnkontak.classList.remove('onpage');
 	btnkeahlian.classList.remove('onpage');
+	nav.classList.toggle('slide');
+	var sRate1 = document.getElementsByClassName('rate1');
+	var sRate2 = document.getElementsByClassName('rate2');
+	var sRate3 = document.getElementsByClassName('rate3');
+	var sRate4 = document.getElementsByClassName('rate4');
+	var sRate5 = document.getElementsByClassName('rate5');
+	for(var i = 0; i < sRate1.length; i++){sRate1[i].classList.remove('r1');}
+	for(var i = 0; i < sRate2.length; i++){sRate2[i].classList.remove('r2');}
+	for(var i = 0; i < sRate3.length; i++){sRate3[i].classList.remove('r3');}
+	for(var i = 0; i < sRate4.length; i++){sRate4[i].classList.remove('r4');}
+	for(var i = 0; i < sRate5.length; i++){sRate5[i].classList.remove('r5');}
 });
 
 btnprestasi.addEventListener('click', function(){
@@ -25,6 +37,17 @@ btnprestasi.addEventListener('click', function(){
 	btnpendidikan.classList.remove('onpage');
 	btnkontak.classList.remove('onpage');
 	btnkeahlian.classList.remove('onpage');
+	nav.classList.toggle('slide');
+	var sRate1 = document.getElementsByClassName('rate1');
+	var sRate2 = document.getElementsByClassName('rate2');
+	var sRate3 = document.getElementsByClassName('rate3');
+	var sRate4 = document.getElementsByClassName('rate4');
+	var sRate5 = document.getElementsByClassName('rate5');
+	for(var i = 0; i < sRate1.length; i++){sRate1[i].classList.remove('r1');}
+	for(var i = 0; i < sRate2.length; i++){sRate2[i].classList.remove('r2');}
+	for(var i = 0; i < sRate3.length; i++){sRate3[i].classList.remove('r3');}
+	for(var i = 0; i < sRate4.length; i++){sRate4[i].classList.remove('r4');}
+	for(var i = 0; i < sRate5.length; i++){sRate5[i].classList.remove('r5');}
 });
 
 btnpendidikan.addEventListener('click', function(){
@@ -33,6 +56,17 @@ btnpendidikan.addEventListener('click', function(){
 	btnpendidikan.classList.add('onpage');
 	btnkontak.classList.remove('onpage');
 	btnkeahlian.classList.remove('onpage');
+	nav.classList.toggle('slide');
+	var sRate1 = document.getElementsByClassName('rate1');
+	var sRate2 = document.getElementsByClassName('rate2');
+	var sRate3 = document.getElementsByClassName('rate3');
+	var sRate4 = document.getElementsByClassName('rate4');
+	var sRate5 = document.getElementsByClassName('rate5');
+	for(var i = 0; i < sRate1.length; i++){sRate1[i].classList.remove('r1');}
+	for(var i = 0; i < sRate2.length; i++){sRate2[i].classList.remove('r2');}
+	for(var i = 0; i < sRate3.length; i++){sRate3[i].classList.remove('r3');}
+	for(var i = 0; i < sRate4.length; i++){sRate4[i].classList.remove('r4');}
+	for(var i = 0; i < sRate5.length; i++){sRate5[i].classList.remove('r5');}
 });
 
 btnkontak.addEventListener('click', function(){
@@ -41,6 +75,17 @@ btnkontak.addEventListener('click', function(){
 	btnpendidikan.classList.remove('onpage');
 	btnkontak.classList.add('onpage');
 	btnkeahlian.classList.remove('onpage');
+	nav.classList.toggle('slide');
+	var sRate1 = document.getElementsByClassName('rate1');
+	var sRate2 = document.getElementsByClassName('rate2');
+	var sRate3 = document.getElementsByClassName('rate3');
+	var sRate4 = document.getElementsByClassName('rate4');
+	var sRate5 = document.getElementsByClassName('rate5');
+	for(var i = 0; i < sRate1.length; i++){sRate1[i].classList.remove('r1');}
+	for(var i = 0; i < sRate2.length; i++){sRate2[i].classList.remove('r2');}
+	for(var i = 0; i < sRate3.length; i++){sRate3[i].classList.remove('r3');}
+	for(var i = 0; i < sRate4.length; i++){sRate4[i].classList.remove('r4');}
+	for(var i = 0; i < sRate5.length; i++){sRate5[i].classList.remove('r5');}
 });
 
 btnkeahlian.addEventListener('click', function(){
@@ -49,4 +94,15 @@ btnkeahlian.addEventListener('click', function(){
 	btnpendidikan.classList.remove('onpage');
 	btnkontak.classList.remove('onpage');
 	btnkeahlian.classList.add('onpage');
+	nav.classList.toggle('slide');
+	var sRate1 = document.getElementsByClassName('rate1');
+	var sRate2 = document.getElementsByClassName('rate2');
+	var sRate3 = document.getElementsByClassName('rate3');
+	var sRate4 = document.getElementsByClassName('rate4');
+	var sRate5 = document.getElementsByClassName('rate5');
+	for(var i = 0; i < sRate1.length; i++){sRate1[i].classList.toggle('r1');}
+	for(var i = 0; i < sRate2.length; i++){sRate2[i].classList.toggle('r2');}
+	for(var i = 0; i < sRate3.length; i++){sRate3[i].classList.toggle('r3');}
+	for(var i = 0; i < sRate4.length; i++){sRate4[i].classList.toggle('r4');}
+	for(var i = 0; i < sRate5.length; i++){sRate5[i].classList.toggle('r5');}
 });
